d2d167fbacd899491936db9f2ffa0113c576a095
ibabushkin <ibabushkin@elewise.com>
2021-12-03 11:37:16 +0400

    install body-parser, refactoring server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,43 +1,38 @@
 'use strict';
 
+const express = require(`express`);
+const app = express();
+const bodyParser = require(`body-parser`);
+
 const {
   DEFAULT_PORT,
-  DEFAULT_ENCODING,
-  STATUS_SUCCESS,
-  STATUS_NOT_FOUND,
-  HEAD_HTML
+  DEFAULT_ENCODING
 } = require(`../../constants`);
-const http = require(`http`);
 const {readFile} = require(`fs/promises`);
 const path = require(`path`);
-
-const onClientConnect = async (request, response) => {
-  try {
-    let content = await readFile(path.join(path.dirname(__filename), `../../mock.json`), {encoding: DEFAULT_ENCODING});
-    content = JSON.parse(content);
-    const responseBody = content.map((item) => `<li>${item.title}</li>`).join(``);
-
-    response.writeHead(STATUS_SUCCESS, HEAD_HTML);
-    response.end(`<ul>${responseBody}</ul>`);
-  } catch (e) {
-    response.writeHead(STATUS_NOT_FOUND, HEAD_HTML);
-    response.end(`<p>Items not found</p>`);
-  }
-};
-
-const httpServer = http.createServer(onClientConnect);
 
 module.exports = {
   name: `--server`,
   run(args) {
     const port = args.shift() || DEFAULT_PORT;
 
-    httpServer.listen(port, () => {
-      console.info(`Server listen to localhost:${port}`);
+    /**
+     * Use middleware json and url encoder
+     */
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
+    app.get(`/posts`, async (req, res) => {
+      try {
+        const content = await readFile(path.join(path.dirname(__filename), `../../mock.json`), {encoding: DEFAULT_ENCODING});
+        res.json(JSON.parse(content));
+      } catch (e) {
+        res.json([]);
+      }
     });
 
-    httpServer.on(`error`, ({message}) => {
-      console.error(`Server error: ${message}`);
+    app.listen(port, () => {
+      console.log(`Server started localhost:${port}`);
     });
   }
 };
