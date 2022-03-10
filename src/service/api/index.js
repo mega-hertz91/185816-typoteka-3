b0c1c2d47b1878ee4abdaa15f083247b1d73c2ea
ibabushkin <ibabushkin@elma-bpm.com>
2022-03-10 20:29:22 +0400

    add data-service for publications entity

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -2,19 +2,20 @@
 
 const {Router} = require(`express`);
 const app = new Router();
-const articlesRouter = require(`./articles`);
+const publicationsRouter = require(`./publications`);
 const searchRouter = require(`./search`);
 const categoriesRouter = require(`./categories`);
 
 /**
  * Include services
  */
-const ArticleService = require(`../data-services/article`);
-const CommentService = require(`../data-services/comment`);
+const PublicationService = require(`../data-service/publication`);
 const CategoryService = require(`../data-services/category`);
 const SearchService = require(`../data-services/search`);
 const posts = require(`../data/posts`);
 const categories = require(`../data/categories`);
+const sequelize = require(`../lib/sequelize`);
+const defineModels = require(`../models/index`);
 
 (async () => {
   /**
@@ -24,9 +25,15 @@ const categories = require(`../data/categories`);
   const categoriesMock = await categories();
 
   /**
+   * Defined models
+   */
+
+  defineModels(sequelize);
+
+  /**
    * Inject routes API
    */
-  articlesRouter(app, new ArticleService(articlesMock), new CommentService(articlesMock));
+  publicationsRouter(app, new PublicationService(sequelize));
   searchRouter(app, new SearchService(articlesMock));
   categoriesRouter(app, new CategoryService(categoriesMock));
 })();
---
+++ src/service/data-service/publication.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class PublicationService {
+  constructor(sequelize) {
+    this._Publication = sequelize.models.Publication;
+  }
+
+  /**
+   * @return {Promise}
+   */
+  getAll() {
+    return this._Publication.findAll();
+  }
+
+  /**
+   * @param {int} id
+   * @return {Promise}
+   */
+  getById(id) {
+    return this._Publication.findByPk(id);
+  }
+
+  /**
+   * {title, announce, description, preview, userId}
+   * @param {object} data
+   */
+  create(data) {
+    return this._Publication.create(data);
+  }
+
+  /**
+   * {title, announce, description, preview, userId}
+   * @param {int} id
+   * @param {object} data
+   * @return {Promise}
+   */
+  update(id, data) {
+    return this._Publication.update(data, {
+      where: {id}
+    });
+  }
+
+  /**
+   * @param {int} id
+   * @return {Promise}
+   */
+  drop(id) {
+    return this._Publication.destroy({
+      where: {id}
+    });
+  }
+}
+
+module.exports = PublicationService;
