52b89ac0cb295e75d402da35e3fc17cb2eb650c3
ibabushkin <ibabushkin@elewise.com>
2022-04-13 17:52:00 +0400

    add production docker-compose

--- src/express/routes/home.js
+++ src/express/routes/home.js
@@ -14,29 +14,28 @@ module.exports = (app) => {
    */
   router.get(`/`, async (req, res) => {
     try {
+      // получаем номер страницы
       let {page = 1} = req.query;
       page = +page;
 
+      // количество запрашиваемых объявлений равно количеству объявлений на странице
       const limit = PUBLICATIONS_PER_PAGE;
 
+      // количество объявлений, которое нам нужно пропустить - это количество объявлений на предыдущих страницах
       const offset = (page - 1) * PUBLICATIONS_PER_PAGE;
-
       const [
         {count, publications},
         categories,
-        comments,
-        hotPublications,
+        comments
       ] = await Promise.all([
         api.getArticles({limit, offset}),
         api.getCategories(true),
-        api.getComments(),
-        api.getArticles({}, true)
+        api.getComments()
       ]);
 
-      const hotArticles = hotPublications
-        .sort((a, b) => b.comments.length - a.comments.length)
-        .slice(0, 4)
-        .filter((item) => item.comments.length > 0);
+      const hotArticles = publications.slice(0, 4).sort((a, b) => {
+        return b.comments.length - a.comments.length;
+      });
 
       const totalPages = Math.ceil(count / PUBLICATIONS_PER_PAGE);
 
@@ -52,8 +51,7 @@ module.exports = (app) => {
       });
     } catch (e) {
       res
-        .status(ResponseStatus.INTERNAL_ERROR)
-        .send();
+        .render(`error/500`);
     }
   });
 
