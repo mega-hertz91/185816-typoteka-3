c44bbc4b7e9623f14ccb5d1ec06ed7dd9ca38f12
ibabushkin <ibabushkin@elma-bpm.com>
2022-04-18 08:32:55 +0400

    Fixed auth middleware

--- src/service/middlewares/auth.js
+++ src/service/middlewares/auth.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const {ResponseStatus} = require(`../../constants`);
+
 module.exports = (UserDataService) => {
   return async (req, res, next) => {
     const currentUser = await UserDataService.getById(req.body.userId);
 
     if (!currentUser) {
       res
-        .status(401)
+        .status(ResponseStatus.UNAUTHORIZED)
         .send(`User unauthorized`);
     } else {
       next();
